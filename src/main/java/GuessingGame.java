6607b3abd371a29c5f5ff0fa46c51a2a24c3e802
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-10 16:08:47 +0000

    added number check

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -7,23 +7,43 @@ public class GuessingGame {
     public static void main(String[] args) {
         // welcome message and request for user to input their range from 0 to the number they choose.
         System.out.println("###############################################################################" + "\n" +
-                            "Welcome to the guessing game! Your first input will choose your guessing range!" + "\n" +
+                            "----------------------Welcome to the guessing game!----------------------------" + "\n" +
+                            "Pick a number and a secret number will be generated between 0 and your num!" + "\n" +
+                            "You've got 3 guesses!" + "\n" +
                             "###############################################################################");
 
         // gets the user to input their range and creates an array from 1 to their max
         Scanner reader = new Scanner(System.in);
         int userRange = reader.nextInt();
-        int[] guessRange = IntStream.range(0, userRange).toArray();
 
         System.out.println("You've selected a range between 0 and " + userRange + "\n" +
-                "A random number will now be generated! Take your first guess!");
+                "A random number will now be generated! What's your first guess of the secret number!");
 
+        // Generates the secret number from 0 to their input
+        Random rand = new Random();
+        int numRand = rand.nextInt(userRange);
 
+        for (int i = 0; i < 3; i++) {
+            Scanner guess = new Scanner(System.in);
+            int userGuess = guess.nextInt();
 
-        Scanner guess = new Scanner(System.in);
-        int userGuess = guess.nextInt();
+            //
+            if (userGuess == numRand) {
+                System.out.println("You got it!");
+                break;
+            } else if (userGuess > numRand) {
+                System.out.println("Too high!");
+            } else if (userGuess < numRand) {
+                System.out.println("Too low!");
+            }
 
-
+            // checks how many guess the user has
+            if (i == 2){
+                System.out.println("Sorry, all out of guesses!");
+            } else {
+                System.out.println("Try again!");
+            }
+        }
 
     }
 }
