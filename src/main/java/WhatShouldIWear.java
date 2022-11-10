b649f4c43858a21d799f393b1df1f8b84c40e5d2
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-10 16:49:53 +0000

    added sorting statements

--- src/main/java/WhatShouldIWear.java
+++ src/main/java/WhatShouldIWear.java
@@ -18,16 +18,26 @@ public class WhatShouldIWear {
 
         // weather and temperature descriptions
         String[] weatherDesc = {"sunny", "rainy", "cloudy", "snowing", "foggy", "windy"};
-        String[] tempDesc = {"freezing", "cold", "chilly", "moderate", "warm", "boiling"};
+        String[] tempDesc = {"freezing", "cold", "chilly", "warm", "boiling"};
 
         // clothing types
-        String[] clothType = {"coat", "trousers", "shorts", "t-shirt"};
+        String[] clothType = {"coat", "trousers", "shorts", "a t-shirt"};
         String[] coatType = {"light", "heavy", "waterproof", "windbreaker"};
 
+        // indices for sorting
+        int weatherInd;
+        int clothInd;
+
         if (temp < 0){
-
+            weatherInd = 0;
         } else if (0 < temp && temp < 9) {
-
+            weatherInd = 1;
+        } else if (10 < temp && temp < 19) {
+            weatherInd = 2;
+        } else if (20 < temp && temp < 29) {
+            weatherInd = 3;
+        } else if (temp > 30) {
+            weatherInd = 4;
         }
 
     }
