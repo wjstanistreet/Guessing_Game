b97ac009f8f84f3802fcfd512cfbce6ac9228616
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-10 15:38:58 +0000

    added guess range

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,20 +1,28 @@
 import jdk.jshell.SourceCodeAnalysis;
-
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.Random;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        // welcome message and request for user to input their range from 1 to the number they choose.
+        // welcome message and request for user to input their range from 0 to the number they choose.
         System.out.println("###############################################################################" + "\n" +
                             "Welcome to the guessing game! Your first input will choose your guessing range!" + "\n" +
                             "###############################################################################");
 
-        // gets the user to input
+        // gets the user to input their range and creates an array from 1 to their max
         Scanner reader = new Scanner(System.in);
-        int guessRange = reader.nextInt();
+        int userRange = reader.nextInt();
+        int[] guessRange = IntStream.range(0, userRange).toArray();
 
-        System.out.println("You've selected a range between 1 and " + guessRange + "\n" +
-                "A random number will now be generated!");
+        System.out.println("You've selected a range between 0 and " + userRange + "\n" +
+                "A random number will now be generated! Take your first guess!");
+
+
+
+        Scanner guess = new Scanner(System.in);
+        int userGuess = guess.nextInt();
+
 
 
     }
