a043ab289bad8435a9bb520ea2175ee245eabbef
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-10 15:21:43 +0000

    added welcome and user input section

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,5 +1,21 @@
+import jdk.jshell.SourceCodeAnalysis;
+
+import java.util.Scanner;
+
 public class GuessingGame {
     public static void main(String[] args) {
-        
+        // welcome message and request for user to input their range from 1 to the number they choose.
+        System.out.println("###############################################################################" + "\n" +
+                            "Welcome to the guessing game! Your first input will choose your guessing range!" + "\n" +
+                            "###############################################################################");
+
+        // gets the user to input
+        Scanner reader = new Scanner(System.in);
+        int guessRange = reader.nextInt();
+
+        System.out.println("You've selected a range between 1 and " + guessRange + "\n" +
+                "A random number will now be generated!");
+
+
     }
 }
